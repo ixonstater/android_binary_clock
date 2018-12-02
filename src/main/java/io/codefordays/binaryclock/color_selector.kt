68764049e3932681c4a8519c5f27194dfa2af9e7
ixonstater <ixonstater@gmail.com>
2018-12-01 21:29:49 -0700

    Set onProgressChanged event listeners to display currently selected color to user.

--- src/main/java/io/codefordays/binaryclock/color_selector.kt
+++ src/main/java/io/codefordays/binaryclock/color_selector.kt
@@ -8,13 +8,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.SeekBar
 import io.codefordays.binaryclock.io.codefordays.model.DataModel
 
 class ColorSelector : DialogFragment() {
 
     private var buttonNum = 0
-    private lateinit var settingsData: SettingsData
+    lateinit var settingsData: SettingsData
+    private lateinit var currentColorDisplay: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +31,10 @@ class ColorSelector : DialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity!!.findViewById<Button>(R.id.color_selector_button).setOnClickListener { applySettings() }
+        activity!!.findViewById<SeekBar>(R.id.seekBarR).setOnSeekBarChangeListener(SeekbarListener(this))
+        activity!!.findViewById<SeekBar>(R.id.seekBarG).setOnSeekBarChangeListener(SeekbarListener(this))
+        activity!!.findViewById<SeekBar>(R.id.seekBarB).setOnSeekBarChangeListener(SeekbarListener(this))
+        currentColorDisplay = activity!!.findViewById(R.id.imageView)
         settingsData = ViewModelProviders.of(this).get(SettingsData::class.java)
         settingsData.pullData()
     }
@@ -40,30 +46,46 @@ class ColorSelector : DialogFragment() {
 
     private fun applySettings(){
         val parentAct = activity as Settings
-        settingsData.setColorSettings(convertColor(), buttonNum)
+        val red = activity!!.findViewById<SeekBar>(R.id.seekBarR).progress * 255 / 100
+        val green = activity!!.findViewById<SeekBar>(R.id.seekBarG).progress * 255 / 100
+        val blue = activity!!.findViewById<SeekBar>(R.id.seekBarB).progress * 255 / 100
+        settingsData.setColorSettings(convertColor(red, green, blue), buttonNum)
         settingsData.pushData()
         parentAct.closeColorSelector()
     }
 
-    private fun convertColor() : Int{
-        var red =  (activity!!.findViewById<SeekBar>(R.id.seekBarR).progress * 255 / 100).toString(16)
-        if(red.length == 1){red = "0$red"}
+    private fun convertColor(red: Int, green: Int, blue: Int) : Int{
+        var redHex =  red.toString(16)
+        if(redHex.length == 1){ redHex = "0$redHex" }
 
-        var green = (activity!!.findViewById<SeekBar>(R.id.seekBarG).progress * 255 / 100).toString(16)
-        if(green.length == 1){green = "0$green"}
+        var greenHex = green.toString(16)
+        if(greenHex.length == 1){greenHex = "0$greenHex"}
 
-        var blue = (activity!!.findViewById<SeekBar>(R.id.seekBarB).progress * 255 / 100).toString(16)
-        if(blue.length == 1){blue = "0$blue"}
+        var blueHex = blue.toString(16)
+        if(blueHex.length == 1){blueHex = "0$blueHex"}
 
-        val hexColor = "#$red$green$blue"
+        val hexColor = "#$redHex$greenHex$blueHex"
+        var retVal = 0
+        try{
+            retVal = Color.parseColor(hexColor)
+        } catch (e: IllegalArgumentException){
+            println(retVal)
+        }
         return Color.parseColor(hexColor)
+    }
+
+    fun updateColorDisplay(){
+        currentColorDisplay.setBackgroundColor(convertColor(settingsData.currentColorR, settingsData.currentColorG, settingsData.currentColorB))
     }
 }
 
-private class SeekbarListener() : SeekBar.OnSeekBarChangeListener{
+private class SeekbarListener(val frag: ColorSelector) : SeekBar.OnSeekBarChangeListener{
+
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
         when(seekBar?.id){
-
+            R.id.seekBarR -> redChanged(progress)
+            R.id.seekBarG -> greenChanged(progress)
+            R.id.seekBarB -> blueChanged(progress)
         }
     }
 
@@ -74,10 +96,28 @@ private class SeekbarListener() : SeekBar.OnSeekBarChangeListener{
     override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
     }
+
+    fun redChanged(progress: Int){
+        frag.settingsData.currentColorR = progress * 255 / 100
+        frag.updateColorDisplay()
+    }
+
+    fun greenChanged(progress: Int){
+        frag.settingsData.currentColorG = progress * 255 / 100
+        frag.updateColorDisplay()
+    }
+
+    fun blueChanged(progress: Int){
+        frag.settingsData.currentColorB = progress * 255 / 100
+        frag.updateColorDisplay()
+    }
 }
 
 class SettingsData : ViewModel(){
     private lateinit var colorSettings: Array<Int>
+    var currentColorR = 255
+    var currentColorG = 255
+    var currentColorB = 255
     fun pushData(){
         DataModel.colorSettings.setColorSettings(colorSettings)
     }
